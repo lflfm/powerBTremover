4b8fc120c9ae244c9a3b1c25087d55b51aa5c537
LFLFM <26166377+lflfm@users.noreply.github.com>
2020-05-14 13:24:27 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# btRemover
+Unpair and remove your Bluetooth device from Windows 10, even when the standard Windows 10 methods won't work or when the device keeps coming back on it's own.  
+This PowerShell script was written by Keith A. Miller and posted on the Microsoft community thread [Unable to remove bluetooth device on windows 10](https://answers.microsoft.com/en-us/windows/forum/windows_10-networking-winpc/unable-to-remove-bluetooth-device-on-windows-10/ea6da83d-583e-4b80-8714-367510879f07?messageId=eacc108d-9d7b-4e82-9280-60916bd702f1&page=14).  
+    
+This version replaces the old C# [btRemover](https://github.com/lflfm/btRemover) that I wrote, which was hard to get and use.  
+  
+## instructions
+1. Download the removeBluetoothDevice.ps1 file
+2. Right-click it and select "**run with powershell**" _alternative - if you get an error like "scripts are disabled in this system", this should work: copy/paste contents of the file into a powershell window_
+3. You should see the list of paired BlueTooth devices, plus their status, class and address.
+4. Choose the one you wish to remove by typing the number in the list
+5. Make a donation to a local non-profit and/or come back to the web and send thanks to Keith A. Miller and myself if it worked... or post your problem if it didn't _(and donate something somewhere anyway, because you can)_
+
+### picture worth 95 words
+![instructions in an image](images/how2use.png?raw=true "Instructions in an image")
---
+++ removeBluetoothDevice.ps1
@@ -0,0 +1,46 @@
+$Source = @"
+	[DllImport("BluetoothAPIs.dll", SetLastError = true, CallingConvention = CallingConvention.StdCall)]
+	[return: MarshalAs(UnmanagedType.U4)]
+	static extern UInt32 BluetoothRemoveDevice(IntPtr pAddress);
+	public static UInt32 Unpair(UInt64 BTAddress) {
+		GCHandle pinnedAddr = GCHandle.Alloc(BTAddress, GCHandleType.Pinned);
+		IntPtr pAddress     = pinnedAddr.AddrOfPinnedObject();
+		UInt32 result       = BluetoothRemoveDevice(pAddress);
+		pinnedAddr.Free();
+		return result;
+	}
+"@
+Function Get-BTDevice {
+	Get-PnpDevice -class Bluetooth |
+		?{$_.HardwareID -match 'DEV_'} |
+			select Status, Class, FriendlyName, HardwareID,
+				# Extract device address from HardwareID
+				@{N='Address';E={[uInt64]('0x{0}' -f $_.HardwareID[0].Substring(12))}}
+}
+
+################## Execution Begins Here ################
+
+$BTR       = Add-Type -MemberDefinition $Source -Name "BTRemover"  -Namespace "BStuff" -PassThru
+$BTDevices = @(Get-BTDevice) # Force array if null or single item
+
+Do {
+	If ($BTDevices.Count) {
+		"`n******** Bluetooth Devices ********`n" | Write-Host
+		For ($i=0; $i -lt $BTDevices.Count; $i++) {
+			('{0,5} - {1} ({2}/{3}/{4})' -f ($i+1), $BTDevices[$i].FriendlyName, $BTDevices[$i].Status, $BTDevices[$i].Class, $BTDevices[$i].Address) | Write-Host
+		}
+		$selected = Read-Host "`nSelect a device to remove (0 to Exit)"
+		If ([int]$selected -in 1..$BTDevices.Count) {
+			'Removing device: {0}' -f $BTDevices[$Selected-1].FriendlyName | Write-Host
+			$Result = $BTR::Unpair($BTDevices[$Selected-1].Address)
+			If (!$Result) {
+				"Device removed successfully." | Write-Host
+			} Else {
+				"Sorry, an error occured." | Write-Host}
+			}
+	} Else {
+		"`n********* No devices found ********" | Write-Host
+	}
+} While (($BTDevices = @(Get-BTDevice)) -and [int]$selected)
+Write-Host "`n`nPress any key to exit...";
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
